a4f1d177b9c15ceeb5bae7852c2972d689f17fb5
Bianeck <thiagobianeck@gmail.com>
2023-03-31 00:28:20 -0300

    Testando o cache da aplicação

---
+++ src/main/java/br/bieniek/com/websocket/cachetest/StringGenerator.java
@@ -0,0 +1,19 @@
+package br.bieniek.com.websocket.cachetest;
+
+import org.springframework.cache.annotation.Cacheable;
+//import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+//@Component
+public class StringGenerator {
+
+    public String buildSimpleString() {
+        return UUID.randomUUID().toString();
+    }
+
+    @Cacheable("cached-string")
+    public String buildCachedString() {
+        return UUID.randomUUID().toString();
+    }
+}
